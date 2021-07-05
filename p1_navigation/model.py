f565be945cedc19966b4832eed9d5bd79288db3f
JamesMcGuigan <james.mcguigan.github@gmail.com>
2021-07-05 21:22:44 +0100

    p1_navigation | model | switch to 4 layer network

--- p1_navigation/model.py
+++ p1_navigation/model.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=32, fc3_units=16):
         """Initialize parameters and build model.
         Params
         ======
@@ -15,16 +16,18 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.seed   = torch.manual_seed(seed)
-        # self.conv1d = nn.Conv1d(state_size, fc1_units, kernel_size=3)
-        self.fc1    = nn.Linear(fc1_units,  fc1_units)
+        self.fc1    = nn.Linear(state_size, fc1_units)
         self.fc2    = nn.Linear(fc1_units,  fc2_units)
-        self.fc3    = nn.Linear(fc2_units,  action_size)
+        self.fc3    = nn.Linear(fc2_units,  fc3_units)
+        self.fc4    = nn.Linear(fc3_units,  action_size)
 
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        # x = F.relu(self.conv1d(state))
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(state))
-        x = self.fc3(x)
+        x = state
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
+        x = self.fc4(x)
+
         return x
