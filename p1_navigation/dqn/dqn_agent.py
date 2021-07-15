p1_navigation | dqn | bugfix code and train to average score = 5

p1_navigation/dqn/dqn_agent.py
@@ -1,12 +1,14 @@
 import os
 import random
 from collections import namedtuple, deque
+from typing import Tuple
 
 import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
 
+from dqn.ReplayBuffer import ReplayBuffer
 from dqn.model import QNetwork
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
@@ -21,7 +23,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed, model_class=QNetwork):
+    def __init__(self, state_size, action_size, model_class=QNetwork, update_type='dqn', seed=0):
         """Initialize an Agent object.
 
         Params
@@ -30,16 +32,20 @@ class Agent():
             action_size (int): dimension of each action
             seed (int): random seed
         """
+        random.seed(seed)
+
         self.state_size  = state_size
         self.action_size = action_size
-        self.seed        = random.seed(seed)
+        self.update_type = update_type
 
         # Q-Network
         self.qnetwork_local  = model_class(state_size, action_size, seed).to(device)
         self.qnetwork_target = model_class(state_size, action_size, seed).to(device)
-        try:                   qnetwork_parameters = self.qnetwork_local.parameters()
-        except Exception as e: qnetwork_parameters = {}; print('self.qnetwork_local.parameters()', e)
-        self.optimizer = optim.Adam(qnetwork_parameters, lr=LR)
+        self.optimizer       = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+
+        # try:                   qnetwork_parameters = self.qnetwork_local.parameters()
+        #     except Exception as e: qnetwork_parameters = {}; print('self.qnetwork_local.parameters()', e)
+        #     self.optimizer = optim.Adam(qnetwork_parameters, lr=LR)
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
@@ -47,8 +53,10 @@ class Agent():
         self.t_step = 0
 
 
+    ### Load / Save Functions ###
+
     @classmethod
-    def from_env(cls, env, seed=0):
+    def get_env_state_action_size(cls, env) -> Tuple[int, int]:
         # env         = UnityEnvironment(file_name="./Banana_Linux/Banana.x86_64")
         brain_name  = env.brain_names[0]
         brain       = env.brains[brain_name]
@@ -57,8 +65,7 @@ class Agent():
 
         state_size  = state.shape[0]                  # state.shape == (37,)
         action_size = brain.vector_action_space_size  # action_size == 4
-        instance    = cls(state_size=state_size, action_size=action_size, seed=seed)
-        return instance
+        return state_size, action_size
 
 
     def load(self, filename):
@@ -78,6 +85,9 @@ class Agent():
             print(f'\n{self.__class__.__name__}.save(): {filename} = {os.stat(filename).st_size/1024:.1f}kb')
         except Exception as e: print(f'{self.__class__.__name__}.save(): {filename} exception: {e}')
 
+
+
+    ### Agent Interaction Functions ###
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
@@ -126,8 +136,8 @@ class Agent():
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         Q_targets      = rewards + (gamma * Q_targets_next * (1 - dones))
         Q_expected     = self.qnetwork_local(states).gather(1, actions)
-        loss           = F.mse_loss(Q_expected, Q_targets)
 
+        loss           = F.mse_loss(Q_expected, Q_targets)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -148,45 +158,3 @@ class Agent():
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_( tau*local_param.data + (1.0-tau)*target_param.data )
-
-
-
-class ReplayBuffer:
-    """Fixed-size buffer to store experience tuples."""
-
-    def __init__(self, action_size, buffer_size, batch_size, seed):
-        """Initialize a ReplayBuffer object.
-
-        Params
-        ======
-            action_size (int): dimension of each action
-            buffer_size (int): maximum size of buffer
-            batch_size (int): size of each training batch
-            seed (int): random seed
-        """
-        self.action_size = action_size
-        self.memory      = deque(maxlen=buffer_size)
-        self.batch_size  = batch_size
-        self.experience  = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed        = random.seed(seed)
-
-    def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
-
-        states      = torch.from_numpy(np.vstack([e.state      for e in experiences if e is not None])).float().to(device)
-        actions     = torch.from_numpy(np.vstack([e.action     for e in experiences if e is not None])).long().to(device)
-        rewards     = torch.from_numpy(np.vstack([e.reward     for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones       = torch.from_numpy(np.vstack([e.done       for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-
-        return (states, actions, rewards, next_states, dones)
-
-    def __len__(self):
-        """Return the current size of internal memory."""
-        return len(self.memory)

p1_navigation/dqn/train.py
@@ -5,15 +5,16 @@ from matplotlib import pyplot as plt
 from unityagents import UnityEnvironment
 
 from dqn.dqn_agent import Agent
+from dqn.model import QNetwork
 
 
 def train_dqn(
-    env, agent, filename,
-    obs_attr='vector_observations',  # vector_observations || visual_observations
-    # obs_attr='visual_observations',  # vector_observations || visual_observations
-    n_episodes=2000, max_t=100, eps_start=1.0, eps_end=0.01, eps_decay=0.995,
-    score_window_size=100, win_score=13,
-    exit_after_first_reward=False,
+        env, agent, filename,
+        obs_attr='vector_observations',  # vector_observations || visual_observations
+        # obs_attr='visual_observations',  # vector_observations || visual_observations
+        n_episodes=2000, max_t=100, eps_start=1.0, eps_end=0.01, eps_decay=0.995,
+        score_window_size=100, win_score=13,
+        exit_after_first_reward=False,
 ):
     """Deep Q-Learning.
 
@@ -41,10 +42,10 @@ def train_dqn(
             'win_score': win_score,
             'exit_after_first_reward': exit_after_first_reward,
         }, ')')
-        
+
         brain_name  = env.brain_names[0]
         brain       = env.brains[brain_name]
-        action_size = brain.vector_action_space_size        # action_size == 4
+        # action_size = brain.vector_action_space_size        # action_size == 4
 
         scores_window = deque(maxlen=score_window_size)  # last 100 scores
         eps = eps_start                    # initialize epsilon
@@ -53,18 +54,19 @@ def train_dqn(
             if obs_attr == 'vector_observations': state = env_info.vector_observations[0]
             if obs_attr == 'visual_observations': state = env_info.visual_observations[0]  # is always empty list
 
-
             score = 0
+            state = getattr(env_info, obs_attr)[0]
             for t in range(max_t):
                 action     = agent.act(state)
                 env_info   = env.step(action)[brain_name]       # send the action to the environment
                 next_state = getattr(env_info, obs_attr)[0]
                 reward     = env_info.rewards[0]                # get the reward
                 done       = env_info.local_done[0]             # see if episode has finished
-                state      = next_state                         # roll over the state to next time step
-                score     += reward
 
                 agent.step(state, action, reward, next_state, done)
+
+                state      = next_state                         # roll over the state to next time step
+                score     += reward
 
                 if exit_after_first_reward and score != 0: break  # keep going until at least one yellow banana
                 if done: break
@@ -72,10 +74,10 @@ def train_dqn(
             scores_window.append(score)       # save most recent score
             scores.append(score)              # save most recent score
             eps = max(eps_end, eps_decay*eps) # decrease epsilon
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+            print('\rEpisode {:4d}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
 
             if i_episode % 100 == 0:
-                print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+                print('\rEpisode {:4d}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
 
             if np.mean(scores_window) >= win_score:
                 print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
@@ -91,7 +93,8 @@ def train_dqn(
 
 if __name__ == '__main__':
     env   = UnityEnvironment(file_name="./Banana_Linux/Banana.x86_64")
-    agent = Agent.from_env(env)  #  state_size == 37, action_size == 4
+    state_size, action_size = Agent.get_env_state_action_size(env)
+    agent = Agent(state_size, action_size, model_class=QNetwork, update_type='dqn')  #  state_size == 37, action_size == 4
     # agent.load('model.pth')
 
     # Increment max_t to assist with finding the first banana